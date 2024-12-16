7f1872d0320f7bb9fa77b73b2c50d329c88c9c09
ricardo <rayrtsx@proton.me>
2024-12-16 16:03:01 -0600

    Update Astro configuration to set output to server and ensure standalone mode for the node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import icon from 'astro-icon';
-
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
 
+  output: "server",
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
