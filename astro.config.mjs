f6c5412ac1fbe76bb88ab9123449d3a5959c6afd
ricardo <rayrtsx@proton.me>
2024-12-15 21:24:03 -0600

    Add @astrojs/node adapter for standalone mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
