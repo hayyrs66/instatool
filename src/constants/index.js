9a538bd3e629259afb72a71c9f6e3588575882a9
hayyrs66 <rayrtsx@proton.me>
2025-01-01 02:18:10 -0600

    Remove unused posts constant from index.js

--- src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const posts = [
-  {
-    id: "",
-    src: "https://instagram.fgua3-4.fna.fbcdn.net/v/t51.29350-15/469634536_588663090212391_6793146795247318280_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDEwODAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.fgua3-4.fna.fbcdn.net&_nc_cat=103&_nc_ohc=vRHvsnZxergQ7kNvgHcwqUE&_nc_gid=ce906b9244a3417fa91ef8dcd3d0c3dc&edm=APs17CUBAAAA&ccb=7-5&oh=00_AYDvrbO8v7J90IL3ZFj7RUhqHeRfwnb3OSxDAhiG_sd7dw&oe=676651B6&_nc_sid=10d13b",
-  },
-];
