2b027db8ddbd1dc4f9483b85c8c1147cd43b3958
hayyrs66 <rayrtsx@proton.me>
2025-01-01 02:17:54 -0600

    Add custom font sizes to Tailwind configuration for responsive design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontSize: {
+        clamp_hero: "clamp(1.875rem, 0.536rem + 3.571vw, 3.75rem)",
+        clamp_p_hero: "clamp(1rem, 0.643rem + 0.952vw, 1.5rem)",
+      },
+    },
+  },
+  plugins: [],
+};
